8e9b6ca47c218ddbcac081bb3618d6ff2dd57885
Zitank <168161873+Zitank@users.noreply.github.com>
2024-05-20 13:42:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-let likeNumber = document.getElementById("skolko") 
-let text = document.getElementById("text"); 
-let post = document.getElementById("firstPost");  
-let postsBox = document.getElementById("postsBox")  
-let comment = document.getElementById("comment")  
-let comments = document.getElementById("comments")  
-let pic = [  
-    "horror.jpg", 
+let likeNumber = document.getElementById("skolko");
+let text = document.getElementById("text");
+let post = document.getElementById("firstPost");
+let postsBox = document.getElementById("postsBox");
+let comment = document.getElementById("comment");
+let comments = document.getElementById("comments");
+let pic = [
+    "horror.jpg",
     "treatb.jpg",
-    "tankodaniil.jpg"  
-]  
-let texty = [  
-    "I`ve been thinking, maybe you and i could partner up", 
-    "Ska-pa-dab-di-bab-di-doo-noo-do", 
+    "tankodaniil.jpg"
+];
+let texty = [
+    "I've been thinking, maybe you and I could partner up",
+    "Ska-pa-dab-di-bab-di-doo-noo-do",
     "Zhestkiye tanki brawl stars vova nike"
-]  
-for(let a = 1; a < 97; a++){   
-    text.innerHTML = texty[i%3]
-    console.log(texty[i%3]) 
-    newpost = post.cloneNode(true);  
-    imgpost = newpost.getElementsByClassName("img")[0]  
-    imgpost.setAttribute("src", pic[i%3])  
-    postsBox.appendChild(newpost);  
-} 
-let c=0;
-function like(){ 
-    c++ 
-    likeNumber.innerHTML = c 
-} 
-function addcomment(){ 
-    let input = comment.value  
-    let addme = document.createElement("p") 
-    addme.innerHTML = "Anonimus:" + input 
-    comments.appendChild(addme) 
+];
+for (let i = 0; i < 97; i++) {
+    text.innerHTML = texty[i % 3];
+    let newpost = post.cloneNode(true);
+    let imgpost = newpost.getElementsByClassName("img")[0];
+    imgpost.setAttribute("src", pic[i % 3]);
+    postsBox.appendChild(newpost);
 }
+let c = 0;
+function like() {
+    c++;
+    likeNumber.innerHTML = c;
+}
+function addcomment() {
+        let input = comment.value;
+        let addme = document.createElement("p");
+        addme.innerHTML = "Anonymous: " + input;
+        comments.appendChild(addme);
+        comment.value = ""; 
+     }
