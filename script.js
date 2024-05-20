52efa366c548068679ad2353f31d9e42ebbce8e5
Zitank <168161873+Zitank@users.noreply.github.com>
2024-05-20 08:24:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let likeNumber = document.getElementById("howmany") 
+let text = document.getElementById("text"); 
 let post = document.getElementById("firstPost");  
 let postsBox = document.getElementById("postsBox")  
-let text = document.getElementById("text"); 
 let comment = document.getElementById("comment")  
 let comments = document.getElementById("comments")  
 let c=0; 
@@ -15,27 +15,20 @@ let textIT = [
     "Ska-pa-dab-di-bab-di-doo-noo-do", 
     "Zhestkiye tanki brawl stars vova nike"
 ]  
-
 for(let i = 1; i < 100; i++){  
     newtext = textIT[i%3] 
     text.innerHTML = newtext 
     console.log(textIT[i%3]) 
- 
     newpost = post.cloneNode(true);  
     imgpost = newpost.getElementsByClassName("img")[0]  
     text.innerHTML = textIT[i%3] 
     imgpost.setAttribute("src", imgurls[i%3])  
- 
- 
     postsBox.appendChild(newpost);  
- 
 } 
- 
 function like(){ 
     c++ 
     likeNumber.innerHTML = c 
 } 
- 
 function addcomment(){ 
     let input = comment.value  
     let addme = document.createElement("p") 
