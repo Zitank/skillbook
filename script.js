9505f22952c0d113bc5d1819d046ef377d82b8fc
Zitank <168161873+Zitank@users.noreply.github.com>
2024-05-20 08:37:04 +0500

    script.js

--- script.js
+++ script.js
@@ -5,24 +5,22 @@ let postsBox = document.getElementById("postsBox")
 let comment = document.getElementById("comment")  
 let comments = document.getElementById("comments")  
 let c=0; 
-let imgurls = [  
+let pic = [  
     "horror.jpg", 
     "treatb.jpg",
     "tankodaniil.jpg"  
 ]  
-let textIT = [  
+let texty = [  
     "I`ve been thinking, maybe you and i could partner up", 
     "Ska-pa-dab-di-bab-di-doo-noo-do", 
     "Zhestkiye tanki brawl stars vova nike"
 ]  
-for(let i = 1; i < 100; i++){  
-    newtext = textIT[i%3] 
-    text.innerHTML = newtext 
-    console.log(textIT[i%3]) 
+for(let a = 1; a < 97; a++){   
+    text.innerHTML = texty[i%3]
+    console.log(texty[i%3]) 
     newpost = post.cloneNode(true);  
     imgpost = newpost.getElementsByClassName("img")[0]  
-    text.innerHTML = textIT[i%3] 
-    imgpost.setAttribute("src", imgurls[i%3])  
+    imgpost.setAttribute("src", pic[i%3])  
     postsBox.appendChild(newpost);  
 } 
 function like(){ 
